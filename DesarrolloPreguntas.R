497ba54fdbec1b1d331f9d2c42140858bb1e1563
Ricardo Matías Albornoz Suárez <ricardo.albornoz97@gmail.com>
2019-09-30 11:38:05 -0300

    edité el script, finalizándolo

--- DesarrolloPreguntas.R
+++ DesarrolloPreguntas.R
@@ -86,10 +86,82 @@ print(paste("cuento",i,"misisipis"))
 
 #Ejercicio 7 : Adapte el código anterior para que imprima los elementos de la lista que se
 #creó anteriormente.
-for(listaDeNumeros in 1 : 100){
-  print(paste("cuento",listaDeNumeros,"misisipis"))
+for(i in listaDeNumeros){print(paste("elemento",i," de listaDeNumeros"))}
 }
 ## al cambiar i por listaDeNumeros, se crea una secuencia correspondiente con los valores de la lista.
 
 #Ejercicio 8 : Usando los condicionales vistas en clases, adapte el código anterior para que
 #muestre si un número es par o impar.
+
+for (i in listaDeNumeros){
+  print(i)
+  if(i%%2==0) {print("par")} else {print("impar")}
+}
+#Al codigo realizado, se prueba con numero 2 y entrega como resultado numero par.
+
+
+##Ejercicio 9 : Suponga que existe un sistema de plebiscitos (resultado SI o NO) con las siguientes reglas: 
+##- El padrón electoral consiste de total personas (número entero). 
+##- Hay dos opciones de voto: SI o NO. La cantidad de votos se representan con las variables votosSI y votosNO (números enteros). 
+##- Considerando que el quórum corresponde al 50% + 1 del padrón electoral: Si hay menos votos que el quórum, pero una de las opciones (SI o NO) tiene una votación mayor o igual al 30%, gana dicha opción. Si no, no hay quórum y gana la opción NO./ Si hay suficientes votos para el quórum, gana la opción con más votos. En caso de empate, gana la opción SI. 
+##- Suponga que no hay votos blancos o nulos. Implemente una función que reciba los parámetros total, votosSI y votosNO, y devuelva un String con la opción ganadora (‘SI’ o ‘NO’).
+
+Determinar_Ganador<-function(total,votosSI,votosNO){
+  if (votosSI+votosNO>total){
+    "votaciones alteradas"
+  }else if (votosSI>=votosNO && votosSI>=0.3*total){
+    paste ("SI")
+  }else {
+    paste ("NO")
+  }
+}
+Determinar_Ganador(100,100,50)
+Determinar_Ganador(100,51,49)
+Determinar_Ganador(100,25,3)
+
+#Al aplicar la funcion se determino que las votaciones estaban alteradas, en el segundo caso gano el "SI" y por ultimo, que gano el "NO".
+
+
+##Ejercicio 10 : Una función es un grupo de instrucciones que toma valores de entradas para generar un resultado. Por ejemplo, si se crea una función que sume dos elementos, a y b, de la siguiente manera:  
+##Suma<-function(a,b){       a+b }  
+##Y luego de esto ejecute lo siguiente en R:  
+##Suma(3,4)  
+##¿Qué resultado genera? Pruébelo con operaciones resta,multiplicación, división.
+
+Resta<-function(a,b){a-b}
+Resta(3,4)
+Multiplicacion<-function(a,b){a*b}
+Multiplicacion(3,4)
+Division<-function(a,b){a/b}
+Division(3,4)
+#El resultado que genera la suma es [1] 7, la resta da [1] -1, la multiplicacion [1] 12 y la division [1] 0.75.
+
+
+##Ejercicio 10 : Cree dos funciones, una que genere la diferencia entre las áreas de dos rectángulos(área sombreada figura 1) y otra de las áreas de un círculo (área sombreada figura 2), como muestran las siguientes figuras:  
+
+#Circulo
+#Anillo: double double -> double
+#Anillo: Diferencia de Areas
+
+AreaAnillo<-function(radio1,radio2){
+  abs((pi*radio1^2)-(pi*(radio2^2)))
+}
+AreaAnillo(7,3)
+AreaAnillo(6,2)
+#Ej.1: AreaAnillo(7.3), da como resultado [1] 125.6637
+#Ej.2: AreaAnillo(6,2), da como resultado [1] 100.531
+
+#Rectangulo:
+AreaRectangulo<-function(base1,altura1,base2,altura2){
+  abs((base1*altura1)-(base2*altura2))
+}
+AreaRectangulo(20,10,10,5)
+# La funcion da como resultado [1] 150
+
+##Ejercicio optativo:
+
+for( i in listaDeNumeros){
+  print(i)
+}
+#La ventaja de este codigo es que da un resultado automaticamente con solo poner el nombre de la lista.
+
